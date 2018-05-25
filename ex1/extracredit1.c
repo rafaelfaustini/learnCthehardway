9e6ec617870677002df2cb15f5aa76db9fbdcf44
Rafael Faustini Costa de Oliveira <rafaeltutoplays@gmail.com>
2018-05-25 15:39:23 -0300

    Exercise 1: Dust Off That Compiler

---
+++ ex1/extracredit1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int i=0;
+	printf("Ex1 Extra Credit line %d\n",i);
+	i++;
+	printf("Ex1 Extra Credit line %d\n",i);
+	i++;
+	printf("Ex1 Extra Credit line %d\n",i);
+	i++;
+	printf("Ex1 Extra Credit line %d\n",i);
+	i++;
+	printf("Ex1 Extra Credit line %d\n",i);
+}
